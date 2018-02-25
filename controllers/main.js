aa4aec1e2c9c680c13be99773c60bbae4370b1eb
Wladimir Reinaga <tekkenucv@gmail.com>
2018-02-25 17:14:29 -0400

    iniciando

---
+++ controllers/main.js
@@ -0,0 +1,9 @@
+﻿/* GET home page. */
+module.exports.index = function(req, res) {
+    res.render('index', { title: 'Express' });
+};
+
+/* GET . */
+module.exports.do = function (req, res) {
+    res.render('redir', { title: 'Redirigir aca' });
+};
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var controlMain = require('../controllers/main');
+
+/* GET home page. */
+router.get('/', controlMain.index);
+
+module.exports = router;
